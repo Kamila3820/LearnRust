8ac61a03fd9604ab3142c66deeb4bb5875c582ca
Thanadol Saojarkaval <98137663+Kamila3820@users.noreply.github.com>
2024-12-19 02:16:59 +0700

    - rusting: lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -3,6 +3,13 @@ fn main() {
     let y: i32 = 6;
 
     println!("{}", largest(&x, &y));
+
+    //Crabby
+    let map1 = "Ancient Map of Sea";
+    let map2 = "New world Minecraft";
+
+    let crabby = longest_map(&map1, &map2);
+    println!("Crabby chose: {}", crabby);
 }
 
 fn largest<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
@@ -10,5 +17,14 @@ fn largest<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
         x
     }else {
         y
+    }
+}
+
+//Crabby'a
+fn longest_map<'a>(map1: &'a str, map2: &'a str) -> &'a str{
+    if map1.len() > map2.len() {
+        map1
+    } else {
+        map2
     }
 }
