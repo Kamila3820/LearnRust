88f6a9787bb83429854ff747ea2990125542c4d9
Thanadol Saojarkaval <98137663+Kamila3820@users.noreply.github.com>
2024-12-19 01:01:47 +0700

    - rusting: fn

--- newfunction/src/main.rs
+++ newfunction/src/main.rs
@@ -2,6 +2,13 @@ fn add(x:i32, y:i32) -> i32 {
     x+y
 }
 
+fn crabby_task(task: &str, duration: i32) -> String {
+    format!("Crabby has successfully done {} in {} minutes!", task, duration)
+}
+
 fn main() {
     println!("{}", add(10, 5));
+
+    let crab = crabby_task("coding", 30);
+    println!("{}", crab);
 }
