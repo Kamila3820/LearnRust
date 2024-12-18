d0fc0078c500c8f6b316cdce43600743e515d763
Thanadol Saojarkaval <98137663+Kamila3820@users.noreply.github.com>
2024-12-19 00:36:40 +0700

    - rusting: controlflow

--- controlflow/src/main.rs
+++ controlflow/src/main.rs
@@ -2,10 +2,22 @@ fn main() {
     //demo if-else
     let x = 5;
 
-    if x > 0{
+    if x == 5{
+        println!("Hello Mr.Five!!!!!")
+    }else if x > 0 {
         println!("x is positive");
     }else{
         println!("x is negative");
+    }
+
+    //demo match
+    let enemy = "slime";
+
+    match enemy {
+        "goblin" => println!("Punch its fucking face!"),
+        "slime" => println!("Use water blast"),
+        "dragon" => println!("Run for cover"),
+        _ => println!("I dont know what to do?"),
     }
 
     //demo loop
@@ -14,9 +26,22 @@ fn main() {
         println!("{}", i);
     }
 
+    //demo loop 2
+    let mut wood = 0;
+
+    loop {
+        wood+=1;
+        println!("Nong fang has got {} woods to build her house.", wood);
+
+        if wood == 10 {
+            println!("!!Collect {} woods for the house success!!", wood);
+            break;
+        }
+    }
+
     //demo while loop 
-    let mut z = 10;
-    while z < 30 {
+    let mut z = 0;
+    while z < 5 {
         println!("z = {}", z);
         z+=1;
     }
